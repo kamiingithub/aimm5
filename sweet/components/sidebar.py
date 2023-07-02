c61d782aeffdd1b0d57333269680b0250d7d62d9
Kami.Lin <yilonglin@trip.com>
2023-07-02 11:34:03 +0800

    init

---
+++ sweet/components/sidebar.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+# import faq
+
+
+def sidebar():
+    with st.sidebar:
+        st.markdown(
+            "# 欢迎来到AI妙妙屋🤖🏠"
+        )
+        st.markdown("---")
+
+        st.markdown(
+            "## How to use\n"
+            "1. Enter your angry words\n"
+            "2. Keep calm and friendly❤\n"
+        )
+
+        st.markdown("---")
+        st.markdown("# About ❤甜言蜜语❤ ")
+        st.markdown(
+            "把带有生气、责备😠语气的话语转换成友善的对话"
+        )
+
+        st.markdown("---")
+        st.markdown("Made by [Kami_Lin]")
+        st.markdown("Powered by gpt-3.5")
+        st.markdown("---")
+
+        # faq()
